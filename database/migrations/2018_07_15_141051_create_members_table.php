b52c3bb39c2f30312b94889ec3e37791a94696a4
bruce <346003541@qq.com>
2018-07-15 22:17:51 +0800

    few more files

---
+++ database/migrations/2018_07_15_141051_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('gender');
+            $table->string('mobile',11);
+            $table->string('email','50');
+            $table->string('address',50);
+            $table->string('post_code','10');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
